342231bfe750ba4ed3f1656a510184a215806c7a
svamsush <161456580+svamsush@users.noreply.github.com>
2024-02-27 16:19:53 +0530

    sln init

---
+++ IntranetPortal.Entities/IPContext.cs
@@ -0,0 +1,27 @@
+﻿using IntranetPortal.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace IntranetPortal.Entities
+{
+    public partial class IPContext :DbContext
+    {
+        public IPContext()
+        {
+
+        }
+        public IPContext(DbContextOptions<IPContext> options) : base(options)
+        {
+            
+        }
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ IntranetPortal.Entities/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntranetPortal.Entities.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string NSTCode { get; set; }
+    }
+}
